086cb817da6c6f53b3b5227e619c1b85e6c1822f
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-04-03 17:53:05 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -8,7 +8,6 @@ public class Persona {
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     Persona(long cedula, String nombre) {
@@ -30,6 +29,7 @@ public class Persona {
     }
 
     Persona(String nombre) {
+        this.cedula = 1;
         this.nombre = "";
         totalPersonas++;
     }
